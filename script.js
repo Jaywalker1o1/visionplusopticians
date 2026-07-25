abc6b3da92e9514cb4f877686df1d68a1e96be88
jaywalker1o1 <edwardmushili1@gmail.com>
2026-07-25 21:39:57 +0200

    save local changes

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFpxegfi6emIrtnPMhzShsz6RfKwfYW30",
+    authDomain: "visionplusoptician-6a798.firebaseapp.com",
+    databaseURL: "https://visionplusoptician-6a798-default-rtdb.firebaseio.com",
+    projectId: "visionplusoptician-6a798",
+    storageBucket: "visionplusoptician-6a798.firebasestorage.app",
+    messagingSenderId: "126248518531",
+    appId: "1:126248518531:web:e06f34733ce54770f4acb6",
+    measurementId: "G-SF7RYZC8DD"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 const serviceButtons = document.querySelectorAll('.service-tabs button');
 const servicePanels = document.querySelectorAll('.service-content');
 
